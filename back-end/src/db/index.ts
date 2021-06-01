16406a6caf2c8447592f0d13cd02f3d780b8f210
杨锐 <yangrui.0@bytedance.com>
2021-06-01 20:39:15 +0800

    fix(db): asdasd

--- back-end/src/db/index.ts
+++ back-end/src/db/index.ts
@@ -3,10 +3,10 @@ import { createConnection } from 'typeorm';
 export const connect = async () => {
   await createConnection({
     type: 'mysql',
-    host: process.env['TYPEORM_HOST'] || 'herserver',
+    host: process.env['TYPEORM_HOST'],
     database: 'hackbook',
     username: process.env['TYPEORM_USERNAME'] || 'root',
-    password: process.env['TYPEORM_PASSWORD'] || 'youling',
+    password: process.env['TYPEORM_PASSWORD'],
     synchronize: true,
     logging: false,
     entities: ['src/models/**/*.ts'],
